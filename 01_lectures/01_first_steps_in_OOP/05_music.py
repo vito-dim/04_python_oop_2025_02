4e22965d084de675f509ec19bdb437b17b307c44
vito-dim <valyo.di@gmail.com>
2025-03-13 18:54:58 +0200

    add LAB:01_first_steps_in_OOP

---
+++ 01_lectures/01_first_steps_in_OOP/05_music.py
@@ -0,0 +1,16 @@
+class Music:
+    def __init__(self, title: str, artist: str, lyrics: str):
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+
+    def print_info(self) -> str:
+        return f'This is "{self.title}" from "{self.artist}"'
+
+    def play(self) -> str:
+        return self.lyrics
+
+
+song = Music("Title", "Artist", "Lyrics")
+print(song.print_info())
+print(song.play())
