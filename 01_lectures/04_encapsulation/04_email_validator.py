17d27a2fb35d125e1778e7bf113e087c0a9668ac
vito-dim <valyo.di@gmail.com>
2025-03-14 11:48:34 +0200

    add LAB: 04_encapsulation

---
+++ 01_lectures/04_encapsulation/04_email_validator.py
@@ -0,0 +1,52 @@
+# class EmailValidator:
+#     def __init__(self, min_length: int, mails: list, domains: list):
+#         self.min_length = min_length
+#         self.mails = mails
+#         self.domains = domains
+#
+#     def __is_name_valid(self, name: str) -> bool:
+#         return True if len(name) >= self.min_length else False
+#
+#     def __is_mail_valid(self, mail: str) -> bool:
+#         return True if mail in self.mails else False
+#
+#     def __is_domain_valid(self, domain: str) -> bool:
+#         return True if domain in self.domains else False
+#
+#     def validate(self, email: str):
+#         name, mail_part = email.split('@')
+#         mail, domain = mail_part.split('.')
+#         if self.__is_name_valid(name) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain):
+#             return True
+#         return False
+
+
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name):
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        name, mail_part = email.split("@")
+        mail, domain = mail_part.split(".")
+
+        return self.__is_name_valid(name) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain)
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
